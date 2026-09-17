039c3812d14bc1fce62182b74ae8868c50915b67
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-17 04:23:17 +0300

    feat: seed append-only audit gaps into command center

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import MasterSystemPlanV1 from "./components/MasterSystemPlanV1";
+import SeedAuditGaps from "./components/SeedAuditGaps";
 import "./home.css";
 
 const featuredCars = [
@@ -11,6 +12,7 @@ const chips=["Всички","BMW","Mercedes","Audi","SUV","Buy Now","До €20 
 
 export default function Home(){return <main className="marketPage referenceMarket">
   <MasterSystemPlanV1 />
+  <SeedAuditGaps />
   <section id="top" className="referenceHero">
     <div className="referenceShade" />
     <header className="referenceHeader">
