bd5244eea25ab349d72ea325116354b33274ea95
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-17 07:42:48 +0300

    style(command-center): load responsive menu overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./enchev-theme.css";
 import "./home-v2.css";
 import "./inventory/inventory-v2.css";
 import "./responsive-grid.css";
+import "./command-center-responsive.css";
 import MouseAura from "./components/MouseAura";
 import BackgroundSwitcher from "./components/BackgroundSwitcher";
 import AiWorkerControl from "./components/AiWorkerControl";
