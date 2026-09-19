261b3c007a8aa5bfbf002ff4b97fac4f58f14d95
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-19 21:28:49 +0300

    feat(search): use 21.20 cross-script matcher in inventory

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { accountNavigation, primaryNavigation } from "../site-navigation";
+import { accountNavigation, primaryNavigation } from "../site-navigation";\nimport { matchesCrossScriptSearch } from "../../packages/config/src/cross-script-search";
 import "./inventory.css";
 import "./inventory-v2.css";
 import "./inventory-d13.css";
