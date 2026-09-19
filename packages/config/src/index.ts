9098672183e97c9ed8647140af7a5e6496a81ea8
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-19 10:01:50 +0300

    feat(config): export regional CDN strategy

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -22,3 +22,4 @@ export * from "./country-legal-profile";
 export * from "./country-document-profile";
 export * from "./market-activation-gate";
 export * from "./country-market-bundle";
+export * from "./regional-cdn-strategy";
