9fe39962abec47c2f606fc829f7ad0117971ec50
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-19 21:28:28 +0300

    feat(global): export 21.20 search matcher

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -26,4 +26,4 @@ export * from "./regional-cdn-strategy";
 export * from "./unicode-normalization";
 export * from "./locale-fallback-chain";
 export * from "./rtl-layout-capability";
-export * from "./international-contact-models";
+export * from "./international-contact-models";\nexport * from "./cross-script-search";\n
