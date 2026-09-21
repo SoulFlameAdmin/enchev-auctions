9386f7795d70a88d6907f9396fff53890d9b8980
Dimitar Veselinov Lambov <soulflame.mitko@gmail.com>
2026-09-21 19:43:34 +0300

    DAVID Mode Center V2.1: GUI-only launcher

--- DAVID_MODE_SELECTOR_V2.cmd
+++ DAVID_MODE_SELECTOR_V2.cmd
@@ -1,3 +1,4 @@
 @echo off
 set "PS=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
-"%PS%" -NoProfile -ExecutionPolicy Bypass -File "D:\ASI\enchev-auctions\DAVID_MODE_SELECTOR_V2.ps1"
+start "" "%PS%" -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "D:\ASI\enchev-auctions\DAVID_MODE_SELECTOR_V2.ps1"
+exit /b 0
